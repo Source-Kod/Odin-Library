f00e7d7fa49127652fc15d077f37fadcd4061cac
Source-Kod <Source-Kod@protonmail.com>
2023-04-28 22:39:16 -0500

    WIP book cards

--- script.js
+++ script.js
@@ -1,40 +1,78 @@
-const myLibrary = [];
+const myLibrary = [
+  {
+    title: 'ted laso',
+    author: 'bbg',
+    pages: 12,
+    read: true,
+  },
+  {
+    title: 'Harry Potter',
+    author: 'J.K Rowling',
+    pages: 500,
+    read: false,
+  },
+];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function createCards(myLibrary) {
+function createCards() {
   myLibrary.forEach((element) => {
     const cardContainer = document.querySelector('#cardContainer');
     const card = document.createElement('div');
     const title = document.createElement('p');
     const author = document.createElement('p');
     const pages = document.createElement('p');
+    const readButton = document.createElement('button');
+    const removeButton = document.createElement('button');
 
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
+    card.appendChild(readButton);
+    card.appendChild(removeButton);
 
     title.textContent = element.title;
     author.textContent = element.author;
     pages.textContent = element.pages;
+    readButton.textContent = 'Mark Read';
+    removeButton.textContent = 'Remove';
 
     cardContainer.appendChild(card);
-    card.classList = 'bg-fuchsia-400 w-1/5 border-2';
+    card.classList = 'flex flex-col items-center bg-fuchsia-400 border-2 h-1/5';
   });
 }
 
-const currentBook = new Book('Harry Potter', 'J.K Rowling', 500);
-const book2 = new Book('Harry Porter', 'bob', 200);
-addBookToLibrary(currentBook);
-addBookToLibrary(book2);
-createCards(myLibrary);
+function removeCards() {
+  const cardContainer = document.querySelector('#cardContainer');
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+}
 
-// console.table(myLibrary);
+function createEvents() {
+  const addBookButton = document.querySelector('#addBook');
+  addBookButton.addEventListener('click', (e) => {
+    e.preventDefault(); // prevent default form submission behavior
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#readToggle');
+    const currentBook = new Book(title, author, pages, read);
+
+    addBookToLibrary(currentBook);
+    removeCards();
+    createCards();
+  });
+}
+
+createEvents();
+createCards();
